8879568cfc40003a3e1176ac5565818aa34638d0
JonasHurlen <55494389+JonasHurlen@users.noreply.github.com>
2019-09-20 15:39:37 -0400

    Proper Commit for the Java UDP
    Committal of the .java files for the UDP

--- UDPReceiver.java
+++ UDPReceiver.java
@@ -1,4 +1,5 @@
 
+
 import java.net.*;
 
 public class UDPReceiver {
@@ -8,15 +9,20 @@ public class UDPReceiver {
 	public static void main( String args[] )
 	{ 
 	      // Check the arguments
+            
+            //Commented out for testing
 	      if( args.length != 1 )
 	      {
 	         System.out.println( "usage: UDPReceiver port" ) ;
 	         return ;
 	      }
+             
 	      try
 	      {
 	         // Convert the argument to ensure that is it valid
 	         int port = Integer.parseInt( args[0] ) ;
+                  //int port = 1226;//test purposes
+                  
 
 	         // Construct the socket
 	         DatagramSocket socket = new DatagramSocket( port ) ;
@@ -28,6 +34,13 @@ public class UDPReceiver {
 	            socket.receive( packet ) ;
 
 	            System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()).trim() ) ;
+                    
+                    //socket = null;
+                    String message = "ACK:" + new String(packet.getData()).trim();
+                    byte[] data = message.getBytes();
+                    
+                    socket.send(new DatagramPacket(data, data.length, packet.getAddress(), packet.getPort()));
+                    
 	        }  
 	     }
 	     catch( Exception e )
